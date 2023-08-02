ff7234a2d41d912945694c7f9ebb588d041cc067
zhanyubi <zhanyubi@tencent.com>
2023-08-02 16:08:27 +0800

    完善开始UI

--- Assets/Scripts/Controllers/UI/StartMenuUIController.cs
+++ Assets/Scripts/Controllers/UI/StartMenuUIController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -9,13 +10,42 @@ namespace TheLastTour.Controller.UI
 {
     public class StartMenuUIController : MonoBehaviour
     {
-        public Button PlayButton;
-        public Button QuitButton;
+        public Button playButton;
+        public Button quitButton;
+        public Image fadeImage;
+
 
         private void Start()
         {
-            PlayButton.onClick.AddListener(() => { Debug.Log("Game Start"); });
-            QuitButton.onClick.AddListener(() => { Debug.Log("Game Quit"); });
+            playButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Game Start");
+                StartCoroutine(LoadGameWithFadeOut("GameScene_Intro"));
+            });
+            quitButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Game Quit");
+#if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+            });
+        }
+
+        private IEnumerator LoadGameWithFadeOut(string sceneName)
+        {
+            float time = 0;
+            while (time < 0.95)
+            {
+                time += Time.deltaTime * 0.5f;
+                fadeImage.color = new Color(0, 0, 0, time);
+                yield return null;
+            }
+
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
         }
     }
 }
