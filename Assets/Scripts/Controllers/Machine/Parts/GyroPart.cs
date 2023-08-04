a37a5d93cd38f9f935ca13558fa49a7935076116
zhanyubi <zhanyubi@tencent.com>
2023-08-04 10:41:08 +0800

    完善陀螺仪组件

--- Assets/Scripts/Controllers/Machine/Parts/GyroPart.cs
+++ Assets/Scripts/Controllers/Machine/Parts/GyroPart.cs
@@ -8,7 +8,7 @@ namespace TheLastTour.Controller.Machine
     {
         Rigidbody _rigidBody;
 
-        Rigidbody RigidBody
+        Rigidbody SimulatorRigidBody
         {
             get
             {
@@ -21,35 +21,57 @@ namespace TheLastTour.Controller.Machine
             }
         }
 
-        readonly PropertyValue<float> _propertyStability = new PropertyValue<float>(0.5f);
+        readonly PropertyValue<float> _propertyAngleStability = new PropertyValue<float>(1f);
+        readonly PropertyValue<float> _propertyAngularVelocityStability = new PropertyValue<float>(0.5f);
+        private float _initRotation;
 
         protected override void InitProperties()
         {
             base.InitProperties();
 
-            Properties.Add(new MachineProperty("Stability", _propertyStability));
-            _propertyStability.OnValueChanged += (f) =>
-            {
-                if (f < 0 || f > 1)
-                {
-                    _propertyStability.Value = Mathf.Clamp(_propertyStability.Value, 0.01f, 0.99f);
-                }
-            };
+            Properties.Add(new MachineProperty("Angle Stability", _propertyAngleStability));
+            Properties.Add(new MachineProperty("Angular Velocity Stability", _propertyAngularVelocityStability));
+
+
+            _initRotation = transform.rotation.eulerAngles.z;
         }
 
         public override void FixedUpdate()
         {
             base.FixedUpdate();
 
-            if (RigidBody && !RigidBody.isKinematic)
+            if (SimulatorRigidBody)
             {
-                // 计算角速度
-                Vector3 angularVelocity = RigidBody.angularVelocity;
-                Debug.Log("angularVelocity: " + angularVelocity);
+                // 根据角度计算力矩,力矩只与角度符号有关
+                float angle = _initRotation - transform.rotation.eulerAngles.z;
+                angle = (angle - 360 * Mathf.FloorToInt((angle + 180f) / 360f)) > 0 ? 1 : -1;
 
-                angularVelocity *= 1 - _propertyStability.Value * Time.fixedDeltaTime;
-                // 局部速度(局部坐标)
-                RigidBody.angularVelocity = angularVelocity;
+                // 根据角速度修正力矩,防止出现抖动
+                float angularVelocity =
+                    transform.InverseTransformDirection(SimulatorRigidBody.angularVelocity).z > 0
+                        ? 1
+                        : -1;
+
+                // Debug.DrawLine(transform.position, transform.position - transform.forward * angularVelocity,
+                //     Color.red);
+                // Debug.DrawLine(transform.position, transform.position + transform.forward * angle, Color.blue);
+
+                float torque = angle * _propertyAngleStability.Value -
+                               angularVelocity * _propertyAngularVelocityStability.Value;
+
+                // Debug.Log(
+                //     "angle:" + angle + "\n" +
+                //     "angularVelocity:" + angularVelocity + "\n" +
+                //     "torque:" + torque
+                // );
+
+                // Vector3 direction = transform.forward;
+                SimulatorRigidBody.AddRelativeTorque(
+                    transform.forward * torque,
+                    ForceMode.Impulse);
+                //
+                // Debug.DrawLine(transform.position, transform.position + transform.forward * 10, Color.red);
+                // Debug.DrawLine(transform.position, transform.position + transform.forward * torque, Color.blue);
             }
         }
     }
