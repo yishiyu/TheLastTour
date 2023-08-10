af6cbd62c23db40bd4fac91b1b390560d6f6a13a
zhanyubi <zhanyubi@tencent.com>
2023-08-10 10:59:42 +0800

    增加铰链组件

---
+++ Assets/Scripts/Controllers/Machine/Parts/HingePart.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TheLastTour.Controller.Machine
+{
+    public class HingePart : MovablePart
+    {
+        public GameObject hingeBase;
+        public GameObject hingeTop;
+
+        public HingeJoint hingePartJoint;
+
+        public override void OnAttached(ISimulator simulator)
+        {
+            base.OnAttached(simulator);
+            hingePartJoint.connectedBody = simulator.GetSimulatorRigidbody();
+        }
+
+        public override void AddPart(PartController part)
+        {
+            // 与普通的 MovablePart 不同, 以 SpringTop 作为连接点
+            attachedParts.Add(part);
+            part.transform.parent = hingeTop.transform;
+            part.OnAttached(this);
+            UpdateSimulatorMass();
+        }
+    }
+}
